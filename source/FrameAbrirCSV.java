2b63f7fda5789f6624cccc116c458e9e1c1e58c8
Benja10500 <benjamin10500@gmail.com>
2021-07-27 23:44:48 -0300

    Cambiando el directorio del FileChooser

--- source/FrameAbrirCSV.java
+++ source/FrameAbrirCSV.java
@@ -5,18 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-/**Esta clase muestra una pantalla para buscar y almacenar el contenido de un archivo CSV, la direccion
-   debe ser valida (referenciar un archivo CSV existente). Dicha pantalla tambien contiene un checkbox
-   ("JSON") y un boton ("Aceptar"). El primero nos permitira decidir si lo visualizamos en formato JSON
-   o tabla, el segundo nos llevara hacia una nueva ventana. Esta nueva ventana por defectoo sera una 
-   instancia de la clase FrameTablaFinal o FramePreConfig.
-   
- * En el caso de que la direccion proporcionada cumpla las condiciones se podra optar por 2 opciones:
- *  1- Seleccionar la generacion directa de un JSON correspondiente al archivo.("JSON" activado + "Aceptar")
- *  2- Poder preconfigurarlo para luego visualizarlo con formato de tabla ("JSON" desactivado + "Aceptar").
- * .
- * 
-*/
+
 public class FrameAbrirCSV extends JFrame{
     private static final int FrameW=350;
     private static final int FrameH=350;
@@ -44,25 +33,13 @@ public class FrameAbrirCSV extends JFrame{
         pack();
         setResizable(false);
     }
-    /**Este es un metodo nexo entre el constructor y los 2 metodos principales, los cuales se encargan de 
-       cargar los componentes a mostrar y de armar el panel. Pero ademas de esto determina si el archivo 
-       seleccionado es del tipo CSV.*/
+    
     public void cargarPaneles(){
         this.isCSV = false;
         cargarComponentes();
         armarPanel();
     }
-    /**Este metodo se encarga de cargar:
-        -un JLabel donde se alojara un mensaje que informa de un error.
-        -un JTextField donde se alojara la direccion ingresada u obtenida.
-        -un JButton llamado "Buscar", el cual crea un JFileChooser para obtener asi la direccion de un
-            archivo CSV.
-        -un JButton llamado "Abrir", el cual (si se cargo una direccion valida) abrira una nueva ventana
-            del tipo FramePreconfigCSV o FrameAbrirCSV segun corresponda.
-        -un JCheckçBox llamado "JSON", que activado y desactivado definira el comportamiento del boton
-            "Abrir". si "JSON" esta activado instancia un objeto de la clase FrameTablaFinal y de lo
-            contrario de la clase FramePreconfigCSV.
-       */
+    
     public void cargarComponentes(){
         
         //Label para el mensaje de error
@@ -98,6 +75,7 @@ public class FrameAbrirCSV extends JFrame{
                 
                 int status;
                 fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new java.io.File("."));
                 fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("Archivo CSV","csv"));
                 status = fileChooser.showOpenDialog(null);
                 if(status == fileChooser.APPROVE_OPTION){
@@ -124,7 +102,7 @@ public class FrameAbrirCSV extends JFrame{
                 }else{
                     isCSV = false;
                 }
-                //Antes de llamar al metodo, asegura si el tipo de archivo es del tipo CSV
+                
                 AbrirCSV();
             }
         });
@@ -133,14 +111,6 @@ public class FrameAbrirCSV extends JFrame{
         this.checkJSON = new JCheckBox("ver como JSON");
         
     }
-    /**Este metodo armara el panel principal de esta ventana, en el cual se alojaran todos los componentes
-       cargados con anterioridad. Estos se ubicaran de la siguiente manera:
-        -JLabel "ingrese diirecicon"
-        -JTextField - JButton "Buscar" - JButton "Abrir"
-        -JCheckBox "JSON"
-        -JLabel mensaje de error
-        
-       */
     private void armarPanel(){
         //Armado del Panel
         JPanel borderPanel = new JPanel(new BorderLayout());
@@ -174,14 +144,7 @@ public class FrameAbrirCSV extends JFrame{
         this.add(mainPanel);
     }
     
-    /**Este metodo se encarga de controlar que el archivo seleccionado sea del tipo CSV para poder 
-       abrirlo y, posteriormente, trabajarlo con normalidad. Dicho control depende de de una variable
-       booleana la cual me indica si es o no es un archivo CSV y ademas un JCheckBox; todo esto para 
-       permitir decidir si instanciar un FrameTablaCSV o un FramePreconfigCSV. 
-       Si el archivo no cumple las cosndiciones correspondiente a un archivo CSV, nos imprimira un mensaje
-       de error.
-       
-       */
+    
     private void AbrirCSV(){
         try{
             this.csvFile = new CSV(this.pathCSV);
